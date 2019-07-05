8213a2cdf80c79b3ffa1915eef93945088a5eaff
moyo <git@moyo.im>
2019-07-05 11:32:26 +0800

    add simple tests

---
+++ tests/Mocked/PlatformSwap.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Platform swap
+ * User: moyo
+ * Date: Jul 03, 2019
+ * Time: 11:31
+ */
+
+namespace Carno\Tracing\Tests\Mocked;
+
+use Carno\Net\Address;
+use Carno\Promise\Promise;
+use Carno\Promise\Promised;
+use Carno\Tracing\Contracts\Carrier;
+use Carno\Tracing\Contracts\Env;
+use Carno\Tracing\Contracts\Platform;
+use Carno\Tracing\Contracts\Protocol;
+use Carno\Tracing\Contracts\Transport;
+use Carno\Tracing\Standard\Span;
+
+class PlatformSwap implements Platform
+{
+    private $transporter = null;
+
+    public function env() : Env
+    {
+        return new class implements Env {
+            public function tags() : array
+            {
+                return ['local' => 'swap'];
+            }
+        };
+    }
+
+    public function joined() : bool
+    {
+        return true;
+    }
+
+    public function leave() : Promised
+    {
+        return Promise::resolved();
+    }
+
+    public function carrier() : Carrier
+    {
+        return null;
+    }
+
+    public function serializer() : Protocol
+    {
+        return new class implements Protocol {
+            public function serialize(Span $span) : string
+            {
+                return json_encode([
+                    'op' => $span->getOperationName(),
+                    'ctx' => $span->getContext(),
+                    'tags' => $span->getTags(),
+                    'logs' => $span->getLogs(),
+                ]);
+            }
+        };
+    }
+
+    public function transporter() : Transport
+    {
+        if ($this->transporter) {
+            return $this->transporter;
+        }
+
+        return $this->transporter = new class implements Transport {
+            private $data = [];
+
+            public function connect(Address $endpoint) : void
+            {
+            }
+
+            public function disconnect() : Promised
+            {
+                return Promise::resolved();
+            }
+
+            public function loading(string $data) : void
+            {
+                $this->data[] = $data;
+            }
+
+            public function flushing() : void
+            {
+            }
+
+            public function data() : array
+            {
+                return $this->data;
+            }
+        };
+    }
+}
---
+++ tests/Utils/SpansCreatorTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * SpansCreator Test
+ * User: moyo
+ * Date: Jul 03, 2019
+ * Time: 10:48
+ */
+
+namespace Carno\Tracing\Tests\Utils;
+
+use Carno\Coroutine\Context;
+use Carno\Tracing\Tests\Mocked\PlatformSwap;
+use Carno\Tracing\Utils\SpansCreator;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Runner\Exception;
+
+class SpansCreatorTest extends TestCase
+{
+    use SpansCreator;
+
+    public function testUninitialized()
+    {
+        $this->assertFalse($this->traced());
+    }
+
+    public function testCreatingNormal()
+    {
+        $this->newSpan(
+            $ctx = new Context,
+            $op = sprintf('name.%d', rand(1000, 9999)),
+            ['t1' => 'v1', 't2' => 'v2'],
+            ['l1' => 'v1', 'l2' => 'v2'],
+            null,
+            null,
+            null,
+            $swap = new PlatformSwap
+        );
+
+        $this->closeSpan($ctx, ['t3' => 'v3']);
+
+        $dat = json_decode($swap->transporter()->data()[0], true);
+
+        $this->assertEquals($op, $dat['op']);
+
+        $this->assertTrue($dat['ctx']['trace.sampled']);
+        $this->assertTrue(!!preg_match('/^[a-z0-9]{32}$/', $dat['ctx']['trace.id']));
+        $this->assertTrue(!!preg_match('/^[a-z0-9]{16}$/', $dat['ctx']['span.id']));
+
+        $this->assertArraySubset([
+            'local' => 'swap',
+            't1' => 'v1',
+            't2' => 'v2',
+            't3' => 'v3',
+        ], $dat['tags']);
+
+        $this->assertArraySubset([
+            'l1' => 'v1',
+            'l2' => 'v2',
+        ], $dat['logs'][0][1]);
+    }
+
+    public function testCreatingError()
+    {
+        $this->newSpan(
+            $ctx = new Context,
+            $op = sprintf('name.%d', rand(1000, 9999)),
+            [],
+            [],
+            null,
+            null,
+            null,
+            $swap = new PlatformSwap
+        );
+
+        $this->errorSpan($ctx, new Exception($msg = sprintf('test.'.rand(1000, 9999))), ['et' => 'yes']);
+
+        $dat = json_decode($swap->transporter()->data()[0], true);
+
+        $this->assertArraySubset([
+            'local' => 'swap',
+            'error' => 'true',
+            'et' => 'yes',
+        ], $dat['tags']);
+
+        $log = $dat['logs'][0][1];
+
+        $this->assertEquals('error', $log['event']);
+        $this->assertEquals(Exception::class, $log['error.kind']);
+        $this->assertEquals($msg, $log['message']);
+    }
+}
